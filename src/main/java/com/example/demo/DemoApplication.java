0be157413d63b789097d6015b2d329c5af81c8f7
GarySu <Gary.Su@hearst.com>
2019-09-18 15:35:18 -0400

    test manual scaling

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,6 +18,11 @@ public class DemoApplication {
 	@GetMapping("/")
 	public String hello(){
 		return "Hello World!";
+	}
+
+	@RequestMapping("/hello")
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + "!";
 	}
 
 }
